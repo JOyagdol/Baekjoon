6e5d312ef12dadbb79635bee5660c4ed06c65589
JOyagdol <86817044+JOyagdol@users.noreply.github.com>
2023-04-01 22:45:26 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int n;
+int dp[1001];
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	dp[1] = 1;
+	dp[2] = 2;
+	cin >> n;
+	for (int i = 3;i <= n;i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]);
+		dp[i] %= 10007;
+	}
+	
+	cout << dp[n];
+}
