cb5050eece681a01698a23c54e3156fb4dc3a4ce
JOyagdol <86817044+JOyagdol@users.noreply.github.com>
2023-06-23 15:41:06 +0900

    [Silver I] Title: 절댓값 힙, Time: 16 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+struct cmp {
+	bool operator()(int a, int b) {
+		if (abs(a) == abs(b)) {
+			return a > b;
+		}
+		return abs(a) > abs(b);
+	}
+};
+
+priority_queue<int, vector<int>, cmp> pq;
+int n;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	int inp;
+	for (int i = 0;i < n;i++) {
+		cin >> inp;
+		if (inp == 0) {
+			if (pq.empty()) {
+				cout << 0 << "\n";
+			}
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(inp);
+		}
+	}
+
+}
